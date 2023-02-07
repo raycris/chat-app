19e8a4f6b5e3054483e4d32de3bc68698acf6498
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-02-07 17:20:40 -0400

    creating server signup route

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,12 @@
 import {config } from "dotenv"
 config()
+
+
 import fastify from "fastify"
+import cors from "@fastify/cors"
 
 const app = fastify()
+app.register(cors, {origin: process.env.CLIENT_URL})
 
 
 app.listen({port: parseInt(process.env.PORT!)})
